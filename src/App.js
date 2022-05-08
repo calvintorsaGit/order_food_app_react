6c7166c0911db40495764bf5abfaaf478b1924b2
Calvin Saragih <calvin.saragih@btpn.com>
2022-05-08 20:01:21 +0700

    feat(header): updating css for header and headerCartButton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import CartProvider from './store/CartProvider';
+import Header from "./components/Header/Header";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <CartProvider>
+        <Header onShowCart={showCartHandler} />
+      </CartProvider>
   );
 }
 
