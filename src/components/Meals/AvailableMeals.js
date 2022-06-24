7a1ef38132c831eeec0c9d32f0dec4f052e402e8
Calvin <calvintorsa@gmail.com>
2022-06-24 11:21:43 +0700

    chore: updated firebase url

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch(
-                'https://react-http-6b4a6.firebaseio.com/meals.json'
+                'https://react-learn-ff16a-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
             );
 
             if (!response.ok) {
